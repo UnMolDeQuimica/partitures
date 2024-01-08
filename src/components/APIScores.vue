bc5ae1ff0ceb502f6dbc69894cd9877d5309eaf7
Miguel Ángel Rodríguez Vidal <unmoldequimica@gmail.com>
2024-01-08 16:21:19 +0100

    Added asset

---
+++ src/components/APIScores.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="hello">
+    <h1>Random Fact:</h1>
+    <button @click="fetchData">Click Me!</button>
+    <li v-for="item in fact">
+    {{item.name}}
+    </li>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      fact: "",
+    };
+  },
+  methods: {
+    fetchData() {
+      const data = fetch('http://127.0.0.1:8000/xml-files', {
+        method: "GET",
+        headers: {
+        },
+      })
+        .then((response) => {
+          response.json().then((data) => {
+            this.fact = data;
+          });
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    },
+  },
+};
+</script>
+<style>
+button {
+padding: 12px 32px;
+font-size: 16px;
+border-radius: 8px;
+}
+</style>
