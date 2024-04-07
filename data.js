6adf8223b5c0fbfbce3807eac2183c190d349ff5
Miguel Ángel Rodríguez Vidal <unmoldequimica@gmail.com>
2024-04-07 19:32:05 +0200

    Added The Seal Lullaby

--- data.js
+++ data.js
@@ -1,5 +1,6 @@
 exampleSheets = {
     "Ay linda amiga!": "https://raw.githubusercontent.com/UnMolDeQuimica/partitures-source/main/assets/ay-linda-amiga.musicxml",
-    "L'Amo de Son Carabassa": "https://raw.githubusercontent.com/UnMolDeQuimica/partitures-source/main/assets/lamo-de-son-carabassa.musicxml",
     "Happy X-Mas War Is Over": "https://raw.githubusercontent.com/UnMolDeQuimica/partitures-source/main/assets/happy-x-mas-war-is-over.musicxml",
+    "L'Amo de Son Carabassa": "https://raw.githubusercontent.com/UnMolDeQuimica/partitures-source/main/assets/lamo-de-son-carabassa.musicxml",
+    "The Seal Lullaby": "https://raw.githubusercontent.com/UnMolDeQuimica/partitures-source/main/assets/the-seal-lullaby.musicxml"
 }
