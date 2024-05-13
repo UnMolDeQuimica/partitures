5b4f92f0cc3406cd01ed2681e762119a6a0a76f9
Miguel Ángel Rodríguez Vidal <unmoldequimica@gmail.com>
2024-05-13 17:42:51 +0200

    Added songs

--- data.js
+++ data.js
@@ -1,6 +1,10 @@
 exampleSheets = {
+    "Que tinguem sort": "https://raw.githubusercontent.com/UnMolDeQuimica/partitures-source/main/assets/que-tinguem-sort.musicxml",
     "Ay linda amiga!": "https://raw.githubusercontent.com/UnMolDeQuimica/partitures-source/main/assets/ay-linda-amiga.musicxml",
     "Happy X-Mas War Is Over": "https://raw.githubusercontent.com/UnMolDeQuimica/partitures-source/main/assets/happy-x-mas-war-is-over.musicxml",
     "L'Amo de Son Carabassa": "https://raw.githubusercontent.com/UnMolDeQuimica/partitures-source/main/assets/lamo-de-son-carabassa.musicxml",
-    "The Seal Lullaby": "https://raw.githubusercontent.com/UnMolDeQuimica/partitures-source/main/assets/the-seal-lullaby.musicxml"
+    "The Seal Lullaby": "https://raw.githubusercontent.com/UnMolDeQuimica/partitures-source/main/assets/the-seal-lullaby.musicxml",
+    "The River She Is Flowing": "https://raw.githubusercontent.com/UnMolDeQuimica/partitures-source/main/assets/the-river-she-is-flowing.musicxml",
+    "Barbara Ann": "https://raw.githubusercontent.com/UnMolDeQuimica/partitures-source/main/assets/barbara-ann.musicxml",
+    "Ide Were Were": "https://raw.githubusercontent.com/UnMolDeQuimica/partitures-source/main/assets/ide-were-were.musicxml"
 }
